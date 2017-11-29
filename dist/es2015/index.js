4424c852df7cab0d275619c4d7811461fadde9ba
Hamed Fathi <noeffect@outlook.com>
2017-11-29 14:12:38 +0330

    updated

--- dist/es2015/index.js
+++ dist/es2015/index.js
@@ -642,21 +642,3 @@ var PasswordMeter = /** @class */ (function () {
     return PasswordMeter;
 }());
 export { PasswordMeter };
-console.log(JSON.stringify(new PasswordMeter({
-    minLength: { value: 5, message: "Hey!, check minLength" },
-    maxLength: { value: 10, message: "Hey!, check maxLength" },
-    uppercaseLettersMinLength: { value: 1, message: "Hey!, check uppercaseLettersMinLength" },
-    lowercaseLettersMinLength: { value: 2, message: "Hey!, check lowercaseLettersMinLength" },
-    numbersMinLength: { value: 1, message: "Hey!, check numbersMinLength" },
-    symbolsMinLength: { value: 1, message: "Hey!, check symbolsMinLength" },
-    mustBe: { value: ['a', '$'], message: "Hey!, check mustBe" },
-    mustNotBe: { value: ['1baA$', '0xaZ$'], message: "Hey!, check mustNotBe" },
-    startsWith: { value: '1', message: "Hey!, check startsWith" },
-    endsWith: { value: '$', message: "Hey!, check endsWith" }
-}, {
-    veryWeak: 40,
-    weak: 80,
-    medium: 120,
-    strong: 180,
-    veryStrong: 200 // 200>=
-}).getScores(['1baAe$', '0xaZ$', 'ERT', '1pwQvF@87$', '12a4A6rx90$'])));
--- index.ts
+++ index.ts
@@ -706,22 +706,3 @@ export class PasswordMeter {
         return { "score": 0, "status": "Empty" };
     }
 }
-
-console.log(JSON.stringify(new PasswordMeter({
-    minLength: { value: 5, message: "Hey!, check minLength" },
-    maxLength: { value: 10, message: "Hey!, check maxLength" },
-    uppercaseLettersMinLength: { value: 1, message: "Hey!, check uppercaseLettersMinLength" },
-    lowercaseLettersMinLength: { value: 2, message: "Hey!, check lowercaseLettersMinLength" },
-    numbersMinLength: { value: 1, message: "Hey!, check numbersMinLength" },
-    symbolsMinLength: { value: 1, message: "Hey!, check symbolsMinLength" },
-    mustBe: { value: ['a', '$'], message: "Hey!, check mustBe" },
-    mustNotBe: { value: ['1baA$', '0xaZ$'], message: "Hey!, check mustNotBe" },
-    startsWith: { value: '1', message: "Hey!, check startsWith" },
-    endsWith: { value: '$', message: "Hey!, check endsWith" }
-}, {
-        veryWeak: 40,    // 1>=   , <40
-        weak: 80,        // 40>=  , <80
-        medium: 120,     // 80>=  , <120
-        strong: 180,     // 120>= , <200
-        veryStrong: 200  // 200>=
-    }).getScores(['1baAe$', '0xaZ$', 'ERT', '1pwQvF@87$', '12a4A6rx90$'])));
